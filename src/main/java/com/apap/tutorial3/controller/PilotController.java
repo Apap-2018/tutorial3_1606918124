cba2c933c0e5fe0ab370900ad6abb88f078d1a53
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-09-26 23:23:41 +0700

    first commit

---
+++ src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -0,0 +1,71 @@
+package com.apap.tutorial3.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apap.tutorial3.model.PilotModel;
+import com.apap.tutorial3.service.PilotService;
+
+@Controller
+public class PilotController {
+	@Autowired
+	private PilotService pilotService;
+
+	@RequestMapping("/pilot/add")
+	public String add(@RequestParam(value = "id", required = true) String id,
+			@RequestParam(value= "licenseNumber", required = true) String licenseNumber,
+			@RequestParam(value= "name", required = true) String name,
+			@RequestParam(value= "flyHour", required = true) int flyHour) {
+		PilotModel pilot = new PilotModel(id, licenseNumber, name, flyHour);
+		pilotService.addPilot(pilot);
+		return "add";
+	}
+
+	@RequestMapping("/pilot/view")
+	public String view(@RequestParam("licenseNumber") String licenseNumber, Model model) {
+		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+
+		model.addAttribute("pilot", archive);
+		return "view-pilot";
+	}
+
+	@RequestMapping("/pilot/viewall")
+	public String viewall(Model model) {
+		List<PilotModel> archive = pilotService.getPilotList();
+
+		model.addAttribute("listPilot", archive);
+		return "viewall-pilot";
+	}
+
+	@RequestMapping(value = {"/pilot/view/license-number", "/pilot/view/license-number/{licenseNumber}"})
+	public String viewPath(@PathVariable Optional<String> licenseNumber, Model model) {
+		if (licenseNumber.isPresent() && pilotService.getPilotDetailByLicenseNumber(licenseNumber.get()) != null) {
+			PilotModel pilot = pilotService.getPilotDetailByLicenseNumber(licenseNumber.get());
+			
+			model.addAttribute("pilot", pilot);
+			return "view-pilot";
+		}
+		else {
+			return "viewpath-pilot-error";
+		}
+	}
+	
+	@RequestMapping("/pilot/update/license-number/{licenseNumber}/fly-hour/{flyHour}​")
+	public String updatePath(@PathVariable String licenseNumber, @PathVariable String flyHour, Model model) {
+		if (licenseNumber != null) {
+			PilotModel pilot = pilotService.updatePilotFlyHourByLicenseNumber(licenseNumber, Integer.parseInt(flyHour));
+			model.addAttribute("flyHour", pilot.getFlyHour());
+			return "update-FlyHour";
+		}
+		else {
+			return "viewpath-pilot-error";
+		}
+	}
+}
---
+++ src/main/java/com/apap/tutorial3/service/PilotInMemoryService.java
@@ -0,0 +1,48 @@
+package com.apap.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.apap.tutorial3.model.PilotModel;
+
+@Service
+public class PilotInMemoryService implements PilotService {
+	private List<PilotModel> archivePilot;
+	
+	public PilotInMemoryService() {
+		archivePilot = new ArrayList<>();
+	}
+	
+	@Override
+	public void addPilot(PilotModel pilot) {
+		archivePilot.add(pilot);
+	}
+	
+	@Override
+	public List<PilotModel> getPilotList() {
+		return archivePilot;
+	}
+
+	@Override
+	public PilotModel getPilotDetailByLicenseNumber(String licenseNumber) {
+		for (PilotModel pilot:archivePilot) {
+			if (pilot.getLicenseNumber().equals(licenseNumber)) {
+				return pilot;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public PilotModel updatePilotFlyHourByLicenseNumber(String licenseNumber, Integer flyHour) {
+		for (PilotModel pilot:archivePilot) {
+			if (pilot.getLicenseNumber().equals(licenseNumber)) {
+				pilot.setFlyHour(flyHour);
+				return pilot;
+			}
+		}
+		return null;
+	}
+	
+	
+}
